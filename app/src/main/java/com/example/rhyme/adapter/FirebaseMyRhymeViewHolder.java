657a75f0ce403a718c5a3f3137130f18f7c0819d
dorcasndungu <ndungumwihaki017@gmail.com>
2022-04-22 00:20:07 +0300

    this adds firebase viewholder

--- app/src/main/java/com/example/rhyme/adapter/FirebaseMyRhymeViewHolder.java
+++ app/src/main/java/com/example/rhyme/adapter/FirebaseMyRhymeViewHolder.java
@@ -12,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.rhyme.R;
 import com.example.rhyme.constants.Constants;
 import com.example.rhyme.lyricsModels.MyRhyme;
-import com.example.rhyme.ui.RhymeFragment;
+
 import com.example.rhyme.ui.SavedRhymesActivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,57 +25,16 @@ import org.parceler.Parcels;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FirebaseMyRhymeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class FirebaseMyRhymeViewHolder extends RecyclerView.ViewHolder {
     View mView;
     Context mContext;
-
+public TextView itemTitle,itemRhyme;
     public FirebaseMyRhymeViewHolder(View itemView) {
         super(itemView);
-        mView = itemView;
-        mContext = itemView.getContext();
-        itemView.setOnClickListener(this);
+
+        itemTitle=itemView.findViewById(R.id.itemTrackName);
+        itemRhyme=itemView.findViewById(R.id.itemArtist);
     }
+}
 
-    public void bindMyRhyme(MyRhyme myRhyme) {
-        TextView itemTrackName = (TextView) mView.findViewById(R.id.itemTrackName);
-        itemTrackName.setText(myRhyme.getTitle());
-    }
-
-    @Override
-    public void onClick(View view) {
-        final ArrayList<MyRhyme> rhymes = new ArrayList<>();
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RHYMES);
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    rhymes.add(snapshot.getValue(MyRhyme.class));
-                }
-
-                int itemPosition = getLayoutPosition();
-
-                AppCompatActivity activity=(AppCompatActivity)view.getContext();
-                RhymeFragment RhymeFragment=new RhymeFragment();
-                activity.getSupportFragmentManager().beginTransaction().replace(R.id.RecyclerView,RhymeFragment).addToBackStack(null).commit();
-
-                Bundle bundle = new Bundle();
-                bundle.putString("positionRhyme",rhymes.get(itemPosition).getRhyme());
-                bundle.putString("positionTitle",rhymes.get(itemPosition).getTitle());
-                bundle.putString("positionPushId",rhymes.get(itemPosition).getPushId());
-
-                RhymeFragment.setArguments(bundle);
-//                Intent intent = new Intent(mContext, SavedRhymesActivity.class);
-//                intent.putExtra("position", itemPosition + "");
-//                intent.putExtra("rhymes", Parcels.wrap(rhymes));
-
-             //   mContext.startActivity(intent);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
-    }
-    }
 
