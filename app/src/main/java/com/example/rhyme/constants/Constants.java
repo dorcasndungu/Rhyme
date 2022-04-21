b6ceec27306a090a4bc62488fa4e5204f856dbf2
dorcasndungu <ndungumwihaki017@gmail.com>
2022-04-22 00:24:46 +0300

    this adds firebase index key

--- app/src/main/java/com/example/rhyme/constants/Constants.java
+++ app/src/main/java/com/example/rhyme/constants/Constants.java
@@ -2,6 +2,8 @@ package com.example.rhyme.constants;
 
 public class Constants {
     public static final String mm_BASE_URL = "https://api.musixmatch.com/ws/1.1/";
-    public static final String mm_API_KEY = "442dc75dee26b9edddad90bb476bb5fb";
-
+    public static final String mm_API_KEY = "0fe09681b8ecfbcfd504a2ef83bac04d";
+    public static final String PREFERENCES_SEARCHQUERY_KEY = "searchQuery";
+    public static final String FIREBASE_CHILD_RHYMES="myRhymes";
+    public static final String FIREBASE_QUERY_INDEX = "index";
 }
